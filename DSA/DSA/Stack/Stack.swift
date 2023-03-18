6d461663db168c6e30fce740e867ab92cfe8d109
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-03-18 13:08:03 +0900

    배열 기반 스택 구현

--- DSA/DSA/Stack/Stack.swift
+++ DSA/DSA/Stack/Stack.swift
@@ -1,3 +1,12 @@
 //  Stack.swift
 
-
+struct Stack<T> {
+    private var array: [T] = []
+    
+    var size: Int { array.count }
+    var isEmpty: Bool { array.isEmpty }
+    var top: T? { array.last }
+    
+    mutating func push(_ newElement: T) { array.append(newElement) }
+    mutating func pop() -> T { array.removeLast() }
+}
