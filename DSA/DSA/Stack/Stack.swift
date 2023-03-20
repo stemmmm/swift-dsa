e04e2c13875d56be27337174acff9b5ab0a92139
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-03-20 20:07:41 +0900

    Update Stack.swift

--- DSA/DSA/Stack/Stack.swift
+++ DSA/DSA/Stack/Stack.swift
@@ -6,7 +6,7 @@ struct Stack<T> {
     var size: Int { array.count }
     var isEmpty: Bool { array.isEmpty }
     var top: T? { array.last }
-    
+
     mutating func push(_ newElement: T) { array.append(newElement) }
     mutating func pop() -> T { array.removeLast() }
 }
