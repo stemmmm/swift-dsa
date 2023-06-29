840b77ce6c26aca19b2361317b303b2be3c0ba4d
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-06-29 17:01:49 +0900

    Early stopping 기법 적용한 bubble sort 구현

--- DSA/DSA/Sorting/BubbleSort.swift
+++ DSA/DSA/Sorting/BubbleSort.swift
@@ -1,13 +1,32 @@
 //  BubbleSort.swift
 
 extension Array where Element: Comparable {
+//    func bubbleSorted() -> [Element] {
+//        var arr = self
+//
+//        for i in 0 ..< arr.count - 1 {
+//            for j in 0 ..< arr.count - 1 - i {
+//                if arr[j] > arr[j + 1] { arr.swapAt(j, j + 1) }
+//            }
+//        }
+//
+//        return arr
+//    }
+    
+    // Early stopping
     func bubbleSorted() -> [Element] {
         var arr = self
+        var swapped = false
         
         for i in 0 ..< arr.count - 1 {
+            swapped = false
             for j in 0 ..< arr.count - 1 - i {
-                if arr[j] > arr[j + 1] { arr.swapAt(j, j + 1) }
+                if arr[j] > arr[j + 1] {
+                    arr.swapAt(j, j + 1)
+                    swapped = true
+                }
             }
+            if !swapped { break }
         }
         
         return arr
