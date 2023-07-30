beb81ab82207e6c916ca6ab1603e4dfa39598c7b
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-07-30 13:50:54 +0900

    upperBound, lowerBound 함수 구현

--- DSA/DSA/Search/BinarySearch.swift
+++ DSA/DSA/Search/BinarySearch.swift
@@ -24,3 +24,31 @@ func binarySearch<T: Comparable>(_ arr: [T], _ target: T, _ range: Range<Int>) -
     else if arr[mid] > target { return binarySearch(arr, target, range.lowerBound ..< mid) }
     else { return binarySearch(arr, target, mid + 1 ..< range.upperBound) }
 }
+
+// target 값을 삽입할 수 있는 최소 인덱스
+func lowerBound(_ arr: [Int], _ target: Int, _ length: Int) -> Int {
+    var left = 0
+    var right = length
+    
+    while left < right {
+        let mid = (left + right) / 2
+        if arr[mid] >= target { right = mid }
+        else { left = mid + 1 }
+    }
+    
+    return left
+}
+
+// target 값을 삽입할 수 있는 최대 인덱스
+func upperBound(_ arr: [Int], _ target: Int, _ length: Int) -> Int {
+    var left = 0
+    var right = length
+    
+    while left < right {
+        let mid = (left + right) / 2
+        if arr[mid] > target { right = mid }
+        else { left = mid + 1 }
+    }
+    
+    return left
+}
