78800a8940c4b7876b2fa1e5c9a91e10a1a62d82
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-07-31 13:54:40 +0900

    지역 변수명 수정 및 오류 수정

--- DSA/DSA/Search/BinarySearch.swift
+++ DSA/DSA/Search/BinarySearch.swift
@@ -1,54 +1,54 @@
 //  BinarySearch.swift
 
 func binarySearch<T: Comparable>(_ arr: [T], _ target: T) -> Int? {
-    var left = 0
-    var right = arr.count - 1
+    var low = 0
+    var high = arr.count - 1
     
-    while left <= right {
-        let mid = (left + right) / 2
+    while low <= high {
+        let mid = (low + high) / 2
         
         if arr[mid] == target { return mid }
-        else if arr[mid] > target { right = mid - 1 }
-        else { left = mid + 1 }
+        else if arr[mid] > target { high = mid - 1 }
+        else { low = mid + 1 }
     }
     
     return nil
 }
 
-func binarySearch<T: Comparable>(_ arr: [T], _ target: T, _ range: Range<Int>) -> Int? {
+func binarySearch<T: Comparable>(_ arr: [T], _ range: Range<Int>, _ target: T) -> Int? {
     let mid = (range.lowerBound + range.upperBound) / 2
 
-    if range.lowerBound > range.upperBound { return nil }
+    if range.lowerBound >= range.upperBound { return nil }
 
     if arr[mid] == target { return mid }
-    else if arr[mid] > target { return binarySearch(arr, target, range.lowerBound ..< mid) }
-    else { return binarySearch(arr, target, mid + 1 ..< range.upperBound) }
+    else if arr[mid] > target { return binarySearch(arr, range.lowerBound ..< mid, target) }
+    else { return binarySearch(arr, mid + 1 ..< range.upperBound, target) }
 }
 
 // target 값을 삽입할 수 있는 최소 인덱스
-func lowerBound<T: Comparable>(_ arr: [T], _ target: T, _ length: Int) -> Int {
-    var left = 0
-    var right = length
+func lowerBound<T: Comparable>(_ arr: [T], _ target: T) -> Int {
+    var low = 0
+    var high = arr.count
     
-    while left < right {
-        let mid = (left + right) / 2
-        if arr[mid] >= target { right = mid }
-        else { left = mid + 1 }
+    while low < high {
+        let mid = (low + high) / 2
+        if arr[mid] >= target { high = mid }
+        else { low = mid + 1 }
     }
     
-    return left
+    return low
 }
 
 // target 값을 삽입할 수 있는 최대 인덱스
-func upperBound<T: Comparable>(_ arr: [T], _ target: T, _ length: Int) -> Int {
-    var left = 0
-    var right = length
+func upperBound<T: Comparable>(_ arr: [T], _ target: T) -> Int {
+    var low = 0
+    var high = arr.count
     
-    while left < right {
-        let mid = (left + right) / 2
-        if arr[mid] > target { right = mid }
-        else { left = mid + 1 }
+    while low < high {
+        let mid = (low + high) / 2
+        if arr[mid] > target { high = mid }
+        else { low = mid + 1 }
     }
     
-    return left
+    return low
 }
